6846c5cfbe98b718be89225fdb42dd348bac227c
Cavin Karema <cavin.mulewa.karema@gmail.com>
2022-08-31 15:01:14 +0300

    Initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+The idea behind JS clinic is to get you up and running with javascript as soon
+as possible. It abstracts away the very basic stuff such as variable declaration
+and function.
+The focus is more on topics that one encounters more frequently such as arrays and DOM.
---
+++ js-arrays/arrays.js
@@ -0,0 +1,26 @@
+// array construction with String Literal with Constructor
+console.log("Welcome to array tutorial");
+
+const Students = new Array(4);
+students = ["Jack", "Dick", "Tom", "Harry"];
+console.log();
+console.log(typeof students);
+
+// using array literal
+const answers = [42, "paper", -3, "plastic", 20.5];
+console.log("Length of arrays >>", answers.length);
+
+// Using an enhanced for loop to access element in an array
+for(let i=0; i<answers.length; i++)
+    console.log(answers[i]);
+
+// modifying array contents
+answers[0] = "Hagler";
+answers[answers.length] = "Liston";
+
+// print out array contents
+console.log("New array items >>");
+for(let i = 0; i < answers.length ; i++)
+    console.log(answers[i]);
+
+
